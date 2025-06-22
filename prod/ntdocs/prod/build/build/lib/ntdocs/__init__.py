45c462964e93076f0fbc03b79105b517ff02d25a
trhieung <nthieu.ngtr@gmail.com>
2025-06-22 22:19:33 +0700

    Remove tracked build/ directories

--- prod/ntdocs/prod/build/build/lib/ntdocs/__init__.py
+++
@@ -1,9 +0,0 @@
-# Extend the package path to support namespace packages
-import pkgutil
-__path__ = pkgutil.extend_path(__path__, __name__)
-
-from .core.ntdocs import NTDocs
-
-__all__ = [
-    "NTDocs"
-]
--- prod/ntproxy/prod/build/build/lib/ntexample/__init__.py
+++
@@ -1,5 +0,0 @@
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
-from .core.ntexample import NTExample
-
-__all__ = ["NTExample"]
